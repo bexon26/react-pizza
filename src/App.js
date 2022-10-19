6c4204326e5aaacafbfd1e639d4f68145c0f9462
bexon <bexonn80@gmail.com>
2022-10-19 12:48:28 +0300

    2 Devide Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import React from 'react';
 
+import Header from './components/Header/Header';
+import Categories from './components/Categories/Categories';
+import Sort from './components/Sort/Sort';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
+
+import './scss/app.scss';
 
 function App() {
   return (
-    <div classname="App">
-      <h1>Hello World</h1>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мескиканская" price={500} />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
