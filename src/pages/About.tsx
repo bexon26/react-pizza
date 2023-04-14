921adc44676cfb4f46e7a5e6aecddc45f843b802
Евгений Цыдренков <TsydrenkovEvgeniy@cryptotech.local>
2023-04-14 15:05:20 +0300

    add adaptive about me

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,79 +1,131 @@
 import React from "react";
+import dishJPG from "../assets/img/dish-20.jpg";
+import checkSVG from "../assets/img/checkMark.svg";
 
 const About = () => {
   return (
     <div>
-      <h1>О нас</h1>
-      <div className="Content-0-3-198 section-text-content">
-        <p>
-          <span className="strong-0-3-194">Delivio </span>– это современный
-          food-tech проект по доставке еды из ресторанов и продуктов из
-          магазинов. Наша цель заключается в том, чтобы максимально
-          автоматизировать и упростить весь цикл доставки ваших заказов, при
-          этом сохраняя качество и доступность любимых блюд.
-        </p>
-        <span className="strong-0-3-194">Почему мы:</span>
-        <ul>
-          <li>
-            <strong>800+</strong> популярных заведений, подключенных к сервису с
-            расширенным радиусом доставки;
-          </li>
-          <li>
-            <strong>13 000+</strong> блюд и <strong>5 000+</strong> товаров,
-            доступных для заказа, а также постоянные обновления меню в
-            заведениях;
-          </li>
-          <li>
-            удобное приложение для iOS и Android, позволяющее отслеживать
-            передвижения курьера в режиме реального времени;
-          </li>
-          <li>
-            бережная доставка заказа в течение 30 минут с момента готовности
-            блюда;
-          </li>
-          <li>опрятные, вежливые и тактичные курьеры;</li>
-          <li>онлайн-оплата на сайте или в приложении;</li>
-          <li>возможность бесконтактной доставки;</li>
-          <li>регулярные совместные акции с ресторанами;</li>
-          <li>возможность предзаказа за 5 дней;</li>
-          <li>
-            мы не списываем деньги до момента завершения заказа. В случае
-            необходимости возврата, деньги могут быть доступны вам мгновенно.
-          </li>
-        </ul>
-        <p>
-          <span className="strong-0-3-194">Как мы работаем:</span>
-        </p>
-        <p>
-          Для того, чтобы совершить заказ, вам просто нужно выбрать интересующий
-          вас ресторан или магазин и добавить понравившиеся блюда и товары в
-          корзину. После оформления, заказ автоматически отправляется на кухню
-          ресторана или комплектовщику магазина, минуя call-центр. Благодаря
-          такой автоматизации процессов, буквально за полчаса свежий круассан,
-          сочный бургер, ароматный вок или все необходимые продукты будут
-          доставлены к вашему столу.
-        </p>
-        <p>
-          Мы уделяем должное внимание качеству доставки и поэтому позаботились о
-          том, чтобы сохранялась нужная температура блюда! У каждого курьера
-          Delivio имеется специальная термосумка с дополнительным блоком для
-          подогрева.
-        </p>
-        <p>
-          Далее - дело за малым! Вам останется лишь принять заказ у курьера и, в
-          предвкушении, открыть наш фирменный пакет и наслаждаться свежей,
-          ароматной, только-только приготовленной любимой едой!
-        </p>
-        <p>
-          Лучшие заведения уже с нами! Самые популярные объекты питания Минска,
-          Бреста, Витебска, Гомеля, Гродно и Могилева уже доступны в нашем
-          сервисе. Многие из заведений стали нашими эксклюзивными партнёрами и
-          доступны в специальном разделе «Только в Delivio». Мы активно работаем
-          над тем, чтобы каждый из вас мог открывать для себя новые блюда и
-          разные кухни прямо у себя дома!
-        </p>
-        <p>Мы готовы сделать вкусным КАЖДЫЙ ваш день!</p>
-        <h3 className="counterpartiesTitle-0-3-195">Партнеры</h3>
+      <div className="container">
+        <h1>О нас</h1>
+        <div className="about">
+          <div className="about-block">
+            <img
+              src={dishJPG}
+              className="about-block__image"
+              alt="Готовая домашняя еда"
+            />
+          </div>
+          <div className="about-block">
+            <h2 className="section__title">Готовая домашняя еда</h2>
+            <p>
+              Вкусное и сбалансированное питание – залог здоровья и хорошего
+              настроения. Наш сервис специально для тех, кто привык питаться
+              настоящей домашней едой, но не готов тратить время на поход в
+              магазин и приготовление пищи.
+            </p>
+            <p>
+              Мы решим эту проблему. Подбирайте подходящий рацион и
+              наслаждайтесь домашней едой без хлопот!
+            </p>
+            <ul className="checklist">
+              <li>Доставка 3 раза в неделю в герметичных контейнерах</li>
+              <li>Штатные курьеры, которых знают постоянные клиенты</li>
+              <li>Самовывоз или бесплатная доставка от 2100р</li>
+              <li>Индивидуальный подход к каждому клиенту</li>
+              <li>Разнообразие рационов</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <h2 className="section__title section__title-center">Почему мы?</h2>
+
+        <div className="motivations">
+          <div className="about-block">
+            <div className="motivations__item">
+              <div className="motivations__icon motivations__icon-1"></div>
+              <div className="motivations__text">
+                Вкусная домашняя еда по ГОСТу
+              </div>
+            </div>
+          </div>
+          <div className="about-block">
+            <div className="motivations__item">
+              <div className="motivations__icon motivations__icon-2"></div>
+
+              <div className="motivations__text">
+                Не нужно ходить в магазин, не нужно готовить
+              </div>
+            </div>
+          </div>
+          <div className="about-block">
+            <div className="motivations__item ">
+              <div className="motivations__icon motivations__icon-3"></div>
+
+              <div className="motivations__text">
+                Собственное производство и профессиональные повара
+              </div>
+            </div>
+          </div>
+          <div className="about-block">
+            <div className="motivations__item">
+              <div className="motivations__icon motivations__icon-4"></div>
+              <div className="motivations__text">
+                Бесплатная и безопасная доставка
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <h2 className="section__title section__title-center">
+          Бесплатная доставка
+        </h2>
+        <div className="section__intro">
+          <p>
+            У нас есть бесплатная доставка в Москве в пределах 5 км от МКАД.
+          </p>
+          <p>
+            Доставка осуществляется пешими курьерами 3 раза в неделю, в вашем
+            распоряжении всегда будет свежая еда.
+          </p>
+        </div>
+        <div className="section__content">
+          <div className="delivery2">
+            <div className="delivery2__grid">
+              <div className="delivery2__item">
+                <div className="delivery2__wrapper">
+                  <div className="delivery2__thumb"></div>
+                  <div className="delivery2__content">
+                    <div className="delivery2__title">
+                      Курьером домой или в офис
+                    </div>
+                    <div className="delivery2__text">
+                      <p>Доставка осуществляется три раза в неделю.</p>
+                      <p>Доставим до двери в удобный двухчасовой интервал.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="delivery2__item">
+                <div className="delivery2__wrapper">
+                  <div className="delivery2__thumb"></div>
+                  <div className="delivery2__content">
+                    <div className="delivery2__title">
+                      Возможность самовывоза
+                    </div>
+                    <div className="delivery2__text">
+                      <p>
+                        Подробности можете уточнить по телефону при заказе, мы
+                        направим ваш заказ в ближайший к вам пункт самовывоза.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
