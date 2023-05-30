b59406c2f67ecea50a89dbd8ddf393a0fb8cc8fc
Евгений Цыдренков <TsydrenkovEvgeniy@cryptotech.local>
2023-05-30 15:00:58 +0300

    create backend

--- app.js
+++ app.js
@@ -1,2 +1,42 @@
-console.log(6
-   )
+import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+
+// mongoose.connect('')
+
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.send("Hello World!");
+});
+
+app.post("/auth/login", (req, res) => {
+  console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "Вася Пупкин",
+    },
+    "secret123"
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("Server OK");
+});
+let a = 5;
+let b = 10;
+console.log(a + b);
