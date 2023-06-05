5131d2efe15ccde8d12d0b5cbb24f6f2450ab797
Your Name <you@example.com>
2023-06-05 23:03:43 +0300

    backend create dish

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ import mongoose from "mongoose";
 
 import {
   registerValidation,
-  loginValidation
+  loginValidation,
+  dishCreateValidation
 } from "./src/backend/validations/validations.js";
 import checkAuth from "./src/backend/utils/checkAuth.js";
 
 import * as UserController from "./src/backend/controllers/UserController.js";
+import * as PostController from "./src/backend/controllers/PostController.js"
 
 mongoose
   .connect(
@@ -28,6 +30,14 @@ app.use(express.json());
 app.post("/auth/login", loginValidation, UserController.login);
 app.post("/auth/register", registerValidation, UserController.register);
 app.get("/auth/me", checkAuth, UserController.getMe);
+
+// app.get("/dish", PostController.getAll);
+// app.get("/dish/:id", PostController.getOne);
+app.post("/dish",checkAuth, dishCreateValidation, PostController.create);
+// app.delete("/dish", PostController.remove);
+// app.patch("/dish", PostController.update);
+
+
 
 app.listen(4444, (err) => {
   if (err) {
--- src/backend/controllers/PostController.js
+++ src/backend/controllers/PostController.js
@@ -1,4 +1,29 @@
-export const create = (req, res) => {
+import DishModel from "../models/Dish.js";
+
+export const create = async (req, res) => {
   try {
-  } catch (error) {}
+    const dish = new DishModel({
+      id:req.body.id,
+      title : req.body.title,
+      description : req.body.description,
+      titleEN : req.body.titleEN,
+      descriptionEN : req.body.descriptionEN,
+      price : req.body.price,
+      category : req.body.category,
+      raiting : req.body.raiting,
+      weight : req.body.weight,
+      imageUrl : req.body.imageUrl,
+      user: req.userId
+    });
+
+    await dish.save();
+
+
+    res.json(dish);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Не удалось создать блюдо",
+    });
+  }
 };
