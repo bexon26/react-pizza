10aae18940c2336d6c1c952554e1975c35b63b72
Евгений Цыдренков <TsydrenkovEvgeniy@cryptotech.local>
2023-06-21 14:27:45 +0300

    start deploy fix eslint 1

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -1,43 +1,43 @@
-import axios from 'axios';
-import React from 'react'
-import { useParams, useNavigate } from 'react-router-dom';
-
+import axios from "axios";
+import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 const FullPizza: React.FC = () => {
-   const [pizza, setPizza] = React.useState<{
-      imageUrl: string;
-      title: string;
-      price: number;
-   }>();
-   const { _id } = useParams();
-   const navigate = useNavigate();
+  const [pizza, setPizza] = React.useState<{
+    imageUrl: string;
+    title: string;
+    price: number;
+  }>();
+  const { _id } = useParams();
+  const navigate = useNavigate();
 
-   React.useEffect(() => {
-      async function fetchPizza() {
-         try {
-            const { data } = await axios.get('https://63527937a9f3f34c373e9fa1.mockapi.io/Items/' + _id)
-            setPizza(data);
-
-         } catch (error) {
-            alert('Ошибка при получении пиццы')
-            navigate('/react-pizza')
-         }
-
+  React.useEffect(() => {
+    async function fetchPizza() {
+      try {
+        const { data } = await axios.get(
+          "https://63527937a9f3f34c373e9fa1.mockapi.io/Items/" + _id
+        );
+        setPizza(data);
+      } catch (error) {
+        alert("Ошибка при получении пиццы");
+     
+        navigate("/react-pizza");
       }
-      fetchPizza();
-   }, [])
-   if (!pizza) {
-      return <>'Загрузка ...'</>
-   }
+    }
+    fetchPizza();
+    // eslint-disable-next-line
+  }, []);
+  if (!pizza) {
+    return <>'Загрузка ...'</>;
+  }
 
-   return (
-
-      <div className='container'>
-         <img src={pizza.imageUrl} />
-         <h2>{pizza.title}</h2>
-         <h4>{pizza.price} ₽</h4>
-      </div>
-   )
-}
+  return (
+    <div className="container">
+      <img src={pizza.imageUrl} alt={pizza.imageUrl}/>
+      <h2>{pizza.title}</h2>
+      <h4>{pizza.price} ₽</h4>
+    </div>
+  );
+};
 
 export default FullPizza;
